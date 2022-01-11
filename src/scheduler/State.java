3a586e968ff8eff6a0d771fec88322c913ee6837
Shakiba <Shakiba@DESKTOP-MQOKLQD>
2022-01-11 16:47:43 +0330

    name, state and onCPUTime attributes are added to Task class

--- src/scheduler/State.java
+++ src/scheduler/State.java
@@ -7,11 +7,12 @@ public class State {
 
     private Queue ready;
     private Queue waiting;
-    private Queue running;
+    private Task running;
 
     public State() {
         this.ready = new LinkedList<Task>();
         this.waiting = new LinkedList<Task>();
-        this.running = new LinkedList<Task>();
     }
+    
+    
 }
